4ae034d71674f751bfb6277cfca361df6d8b742f
Phillip Mueller <86968936+Phillip-M97@users.noreply.github.com>
2024-05-24 18:51:39 +0200

    Delete download_weights.sh

--- download_weights.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p weights
-# Ours - SDv1-5
-wget http://diffusion_hyperfeatures.berkeleyvision.org/weights/aggregation_network.pt -O weights/aggregation_network.pt
-# Ours - SDv1-5 (One-Step)
-wget http://diffusion_hyperfeatures.berkeleyvision.org/weights/aggregation_network_one-step.pt -O weights/aggregation_network_one-step.pt
-# Ours - SDv2-1
-wget http://diffusion_hyperfeatures.berkeleyvision.org/weights/aggregation_network_sdv2-1.pt -O weights/aggregation_network_sdv2-1.pt
